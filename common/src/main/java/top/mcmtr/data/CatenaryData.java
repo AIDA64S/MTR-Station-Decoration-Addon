14ffc92a32d6fcb4e06a54a8bbe911451880cfbc
AIDA64 <zxc17634985169@gmail.com>
2023-07-19 10:30:42 +0800

    add Catenary help text

--- common/src/main/java/top/mcmtr/data/CatenaryData.java
+++ common/src/main/java/top/mcmtr/data/CatenaryData.java
@@ -38,6 +38,9 @@ public class CatenaryData extends LineDataBase {
         catenaryDataFileSaveModule = new CatenaryDataFileSaveModule(world, catenaries, savePath);
     }
 
+    /**
+     * 此方法会被按照Tick调用，用于将用户设定的视野范围内的本地数据同步到客户端需要渲染的一侧
+     * */
     public void simulateCatenaries() {
         final List<? extends Player> players = world.players();
         players.forEach(player -> {
