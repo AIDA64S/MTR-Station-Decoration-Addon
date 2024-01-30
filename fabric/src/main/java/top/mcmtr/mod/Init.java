dbf9376de38de5dacabb2252db049199a380b188
AIDA64S <zxc17634985169@gmail.com>
2024-01-30 15:29:14 +0800

    add catenaryMath

--- fabric/src/main/java/top/mcmtr/mod/Init.java
+++ fabric/src/main/java/top/mcmtr/mod/Init.java
@@ -1,8 +1,10 @@
 package top.mcmtr.mod;
 
+import java.util.logging.Logger;
+
 public class Init {
     public static final String MOD_ID = "msd";
-
+    public static final Logger MSD_LOGGER = Logger.getLogger("Station-Decoration");
     public static void init() {
     }
 }
