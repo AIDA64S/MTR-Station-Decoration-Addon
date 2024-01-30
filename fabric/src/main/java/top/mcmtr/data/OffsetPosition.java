0df032b0f3dbb5f2340ab520e8aea8a1f9617be3
AIDA64S <zxc17634985169@gmail.com>
2024-01-30 20:47:45 +0800

    update libs

---
+++ fabric/src/main/java/top/mcmtr/data/OffsetPosition.java
@@ -0,0 +1,11 @@
+package top.mcmtr.data;
+
+public class OffsetPosition {
+    public final double x, y, z;
+
+    public OffsetPosition(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
