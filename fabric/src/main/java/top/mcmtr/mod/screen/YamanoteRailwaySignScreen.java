8679ae62e9977f8d053dd6e972b45a88ef45fdb3
Hikari <zxc17634985169@gmail.com>
2025-03-25 22:29:11 +0800

    bump version

--- fabric/src/main/java/top/mcmtr/mod/screen/YamanoteRailwaySignScreen.java
+++ fabric/src/main/java/top/mcmtr/mod/screen/YamanoteRailwaySignScreen.java
@@ -61,7 +61,7 @@ public class YamanoteRailwaySignScreen extends ScreenExtension implements IGui {
             routesForList = new ObjectArraySet<>();
         } else {
             exitsForList = new ObjectImmutableList<>(EditStationScreen.getExitsForDashboardList(EditStationScreen.getStationExits(station, true)));
-            platformsForList = PIDSConfigScreen.getPlatformsForList(station);
+            platformsForList = PIDSConfigScreen.getPlatformsForList(new ObjectArrayList(station.savedRails));
 
             final ObjectArraySet<Station> connectingStationsIncludingThisOne = new ObjectArraySet<>(station.connectedStations);
             connectingStationsIncludingThisOne.add(station);
